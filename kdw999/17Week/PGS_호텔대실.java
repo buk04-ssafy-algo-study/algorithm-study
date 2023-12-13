b647eb57cfa8f9e1f213ed6996b634b6dc404bbb
KDW999 <84887939+KDW999@users.noreply.github.com>
2023-12-13 17:22:59 +0900

    17, 18Week fix

--- kdw999/17Week/PGS_호텔대실.java
+++ kdw999/17Week/PGS_호텔대실.java
@@ -1,10 +1,35 @@
-package Week17;
+import java.util.*;
 
-public class PGS_호텔대실 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Solution {
+	static int maxTime = 1450;
+	static int hour = 60;
+	static int cleanTime = 10;
+	
+	public int solution(String[][] book_time) {
+        int answer = 0;
+        
+        int[] room = new int[maxTime];
+        
+        for(String[] time : book_time) {
+        	String checkIn = time[0];
+        	String checkOut = time[1];
+        	
+        	room[calTime(checkIn)] += 1;
+        	room[calTime(checkOut)+ cleanTime] += -1;
+        }
+        
+        for(int i=1; i<maxTime; i++) {
+        	room[i] += room[i-1];
+        	answer = Math.max(answer, room[i]); // 최대로 누적된 수 만큼 방이 필요
+        }
+        
+        return answer;
+    }
+	
+	public static int calTime(String time) {
+		String[] split = time.split(":"); // 입실, 퇴실 시간 쪼개기
+		String h = split[0];
+		String m = split[1];
+		return (Integer.parseInt(h) * hour) + Integer.parseInt(m);
 	}
-
 }
