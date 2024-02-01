bfaf236a7c44d290c866a1cd2f27f7aaa83db890
KDW999 <84887939+KDW999@users.noreply.github.com>
2024-02-01 19:56:04 +0900

    백준_25047_사각형게임.java

--- kdw999/25Week/백준_25047_사각형게임.java
+++ kdw999/25Week/백준_25047_사각형게임.java
@@ -1,68 +1,74 @@
-package Week25;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.*;
-import java.io.*;
-
-public class 백준_25047_사각형게임 {
-
-	static int N, maxScore;
-	static int[][] map;
-	static int[] input;
-	static int[] pick;
+public class Main {
+	//https://www.acmicpc.net/problem/25046
+	//https://www.acmicpc.net/problem/25047
+	
+	static int N;
+	static long[][] Board;
+	static final long INF = Long.MAX_VALUE;
+	
+	public static long calScore(long minwoo) {
+		//민우 점수
+		long score = 0;
+		//열 탐색
+		for(int i=0;i<N;i++) {
+			//칠한 점수
+			long onScore = 0;
+			//안 칠한 점수
+			long offScore = 0;
+			//행탐색
+			for(int j=0;j<N;j++) {
+				int idx = 1<<j;
+				//색을 칠한 경우
+				if((minwoo&idx)==idx){
+					onScore+=Board[j][i];
+				//안 칠한 경우
+				}else {
+					offScore+=Board[j][i];
+				}
+			}
+			//종진이가 색을 칠하면 offScore를,
+			//색을 칠하지 않으면 onScore를 얻음 
+			//큰 값은 종진이가, 작은 값은 민우가 얻음
+			score+=Math.min(onScore, offScore);
+		}
+		//민우 점수 리턴
+		return score;
+	}
+	
+	public static long getMaxScore() {
+		//점수를 -INF로 초기화
+		long score=-INF;
+		//완전탐색, 비트마스크 사용
+		long bitMask = 1<<N;
+		//행 칠하는 경우의 수
+		for(long i=0;i<bitMask;i++) {
+			//최대 점수를 취함
+			score=Math.max(score, calScore(i));
+		}
+		//출력
+		return score;
+	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		maxScore = 0;
-		
-		input = new int[N+1];
-		for(int i=0; i<=N; i++) {
-			input[i] = i;
-		}
-		map = new int[N][N];
-		
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine()); //게임판 크기
+		Board=new long[N][N];
+		StringTokenizer st;
+		//배열 입력
+		for(int i=0;i<N;i++) {
+			st=new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				Board[i][j]=Long.parseLong(st.nextToken());
 			}
 		}
-		
-		int rotate = N;
-		int cnt = 0;
-		int pickNum = 0;
-		while(rotate >= 0) {
-			
-			pick = new int[cnt+1];
-			combi(cnt, 0, pickNum);
-			cnt++;
-			pickNum++;
-		}
-
-	}
-	
-	public static void combi(int cnt, int start, int pickNum) {
-		
-		
-		// 필요 갯수 뽑으면 해당 인덱스에 맞게 배열의 행들을 칠하고 그 때 민우가 얻는 점수 체크
-		if(cnt == pickNum) {
-		
-			int minuScore = 0;
-			
-			int[][] drawing = new int[N][N];
-			
-			for()
-			
-			return;
-		}
-	
-		
-		for(int i=start; i<=N; i++) {
-			
-			pick[cnt] = input[i];
-			
-			combi(cnt+1, i+1, pickNum);
-		}
+		//출력
+		System.out.println(getMaxScore());
+		br.close();
 	}
 }
