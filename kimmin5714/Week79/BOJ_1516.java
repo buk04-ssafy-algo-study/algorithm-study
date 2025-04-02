2d1ec69815b88cb1fc6e8196c789c8bfd976c4c5
kimmin5714 <kimmin5714@gmail.com>
2025-04-03 02:46:51 +0900

    BOJ_1516

---
+++ kimmin5714/Week79/BOJ_1516.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1516 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[N+1]; // 진입차수
+        int[] buildTime = new int[N+1]; // 건물 짓는데 걸리는 시간
+        int[] resultTime = new int[N+1]; // 선행 건물 짓는데 걸리는 시간
+
+        for(int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            buildTime[i] = Integer.parseInt(st.nextToken());
+
+            while (true) {
+                int prev = Integer.parseInt(st.nextToken());
+                if (prev == -1) break;
+
+                adj.get(prev).add(i);
+                indegree[i]++; // 선행되어야하는 건물 수
+            }
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i = 1; i <= N; i++) {
+            if(indegree[i] == 0) { // 진입 차수가 0인 것 큐에 넣기
+                q.offer(i);
+                resultTime[i] = buildTime[i]; // 다른 건물 지을 필요 없음
+            }
+        }
+
+        while(!q.isEmpty()) {
+
+            int cur = q.poll();
+
+            for(int prev : adj.get(cur)) { // 인접한 노드들 진입차수 감소
+                indegree[prev]--;
+
+                // 가장 오래 걸리는 선행 건물이 끝난 다음에 시작할 수 있음
+                resultTime[prev] = Math.max(resultTime[prev], resultTime[cur] + buildTime[prev]);
+
+                if(indegree[prev] == 0) { // 진입차수가 0이면 큐에 추가
+                    q.offer(prev);
+                }
+            }
+        }
+
+        for(int i = 1; i <= N; i++) {
+            System.out.println(resultTime[i]);
+        }
+    }
+}
