1d391684c0109adc78c5c0908f476f97dafa77e3
kimmin5714 <kimmin5714@gmail.com>
2025-04-03 11:43:50 +0900

    BOJ_1516 수정

--- kimmin5714/Week79/BOJ_1516.java
+++ kimmin5714/Week79/BOJ_1516.java
@@ -11,9 +11,9 @@ public class BOJ_1516 {
             adj.add(new ArrayList<>());
         }
 
-        int[] indegree = new int[N+1]; // 진입차수
+        int[] indegree = new int[N+1]; // 진입차수 => 선행건물
         int[] buildTime = new int[N+1]; // 건물 짓는데 걸리는 시간
-        int[] resultTime = new int[N+1]; // 선행 건물 짓는데 걸리는 시간
+        int[] resultTime = new int[N+1]; // 건물 짓는데 걸리는 최소 시간
 
         for(int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -40,14 +40,14 @@ public class BOJ_1516 {
 
             int cur = q.poll();
 
-            for(int prev : adj.get(cur)) { // 인접한 노드들 진입차수 감소
-                indegree[prev]--;
+            for(int next : adj.get(cur)) { // cur이 선행되었을 때 지을 수 있는 건물들
+                indegree[next]--;
 
                 // 가장 오래 걸리는 선행 건물이 끝난 다음에 시작할 수 있음
-                resultTime[prev] = Math.max(resultTime[prev], resultTime[cur] + buildTime[prev]);
+                resultTime[next] = Math.max(resultTime[next], resultTime[cur] + buildTime[next]);
 
-                if(indegree[prev] == 0) { // 진입차수가 0이면 큐에 추가
-                    q.offer(prev);
+                if(indegree[next] == 0) { // 진입차수가 0이면 큐에 추가
+                    q.offer(next);
                 }
             }
         }
