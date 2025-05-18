9c9a17855c93ef77b0b49ce4c4b60df19827dd76
kimmin5714 <kimmin5714@gmail.com>
2025-05-18 15:53:20 +0900

    BOJ_2636 수정

--- kimmin5714/Week82/BOJ_2636.java
+++ kimmin5714/Week82/BOJ_2636.java
@@ -13,8 +13,7 @@ class Point {
 }
 
 public class BOJ_2636 {
-    static int arr[][], r, c;
-    static List<Point> list;
+    static int arr[][], r, c, remainCheeseCnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,40 +22,21 @@ public class BOJ_2636 {
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         arr = new int[r][c];
+        remainCheeseCnt = 0;
 
         for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < c; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j]==1) remainCheeseCnt++;
             }
-        }
-
-        // 초기 테두리 체크
-        for (int i = 0; i < c; i++) {
-            arr[0][i] = 2;
-            arr[r - 1][i] = 2;
-        }
-        for (int j = 0; j < r; j++) {
-            arr[j][0] = 2;
-            arr[j][c - 1] = 2;
         }
 
         int cheeseCnt = -1;
         int cnt = 0;
-        while (true) {
-            list = new ArrayList<>();
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) {
-                    if (arr[i][j] == 1) {
-                        bfs(i, j); // 해당 칸이 외부랑 연결되면(녹는 치즈) 리스트에 추가
-                    }
-                }
-            }
-            if (list.size() == 0) break;
-            cheeseCnt = list.size();
-            for (int i = 0; i < list.size(); i++) { // 치즈 녹음
-                arr[list.get(i).r][list.get(i).c] = 0;
-            }
+        while (remainCheeseCnt > 0) {
+            cheeseCnt = remainCheeseCnt;
+            bfs(0,0);
             cnt++;
         }
 
@@ -69,6 +49,7 @@ public class BOJ_2636 {
         boolean[][] visited = new boolean[r][c];
         int[] delr = new int[]{-1, 1, 0, 0};
         int[] delc = new int[]{0, 0, -1, 1};
+        int meltCnt = 0;
 
         q.offer(new Point(sr, sc));
         visited[sr][sc] = true;
@@ -80,18 +61,17 @@ public class BOJ_2636 {
                 int nr = cur.r + delr[i];
                 int nc = cur.c + delc[i];
 
-                if(nr<0 || nr>=r || nc<0 || nc>=c || visited[nr][nc]
-                || arr[nr][nc] == 1) continue;
+                if(nr<0 || nr>=r || nc<0 || nc>=c || visited[nr][nc]) continue;
 
-                if(arr[nr][nc] == 2) {
-                    list.add(new Point(sr,sc));
-                    return;
+                if(arr[nr][nc] == 1) {
+                    meltCnt++;
+                    arr[nr][nc] = 0;
+                }else {
+                    q.offer(new Point(nr, nc));
                 }
-
-                q.offer(new Point(nr, nc));
                 visited[nr][nc] = true;
             }
         }
-
+        remainCheeseCnt -= meltCnt;
     }
 }
